eab804a6d5c4e65e9d9234aa7aced235c0216ea5
Theo <theodorosladasmail@gmail.com>
2022-06-10 19:32:07 +0100

    Added - Burger navigation functionality and Mobile responsive code

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+// Accordion Functionality
 // Select all accordion-containers
 const accordionContainers = document.querySelectorAll(".accordion-container");
-
 // Loop through each accordion , create event listener and pass the active class
 accordionContainers.forEach((accordionContainer) => {
   accordionContainer.addEventListener("click", () => {
     accordionContainer.classList.toggle("active");
   });
 });
+
+// Burger Navigation functionality
+// Create the function
+const navSlide = () => {
+  // Selectors
+  const nav = document.querySelector(".nav-links");
+  const burger = document.querySelector(".burger");
+  // Add event listener and toggle classes
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("toggle-burger-lines");
+    nav.classList.toggle("nav-active");
+  });
+};
+// Call the function
+navSlide();
